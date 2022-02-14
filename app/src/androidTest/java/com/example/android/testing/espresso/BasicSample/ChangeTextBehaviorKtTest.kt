26fe1ff77bdc56a307fffc34791ee51e92ddd562
Nils Levandovskis <nils.levandovskis@testdevlab.com>
2022-02-14 14:11:22 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/ChangeTextBehaviorKtTest.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.testing.espresso.BasicSample
+
+import androidx.test.ext.junit.rules.activityScenarioRule
+import android.app.Activity
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * The kotlin equivalent to the ChangeTextBehaviorTest, that
+ * showcases simple view matchers and actions like [ViewMatchers.withId],
+ * [ViewActions.click] and [ViewActions.typeText], and ActivityScenarioRule
+ *
+ *
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class ChangeTextBehaviorKtTest {
+    // Declare static string variable "Espresso"
+    @get:Rule var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun changeText_sameActivity() {
+
+        // 1. Type text and then press the button.
+
+        // 2. Check that the text was changed.
+    }
+
+    @Test
+    fun changeText_newActivity() {
+        // 1. Type text and then press the button.
+
+        // 2. This view is in a different Activity, no need to tell Espresso.
+    }
+}
